b696b40934a93a64c0ef75772ad662a48bd6d91e
AYkimchi2000 <136215568+AYkimchi2000@users.noreply.github.com>
2024-11-14 17:54:05 +0800

    added additional 2 cases according to assignment requirement

--- homework3.py
+++ homework3.py
@@ -125,6 +125,8 @@ def z_score(x, mu, sigma):
         print("standard deviation can't be 0! review dataset to non identical!")
     return zscore # Place the calculated z-score result between the return statement and this comment so it will be returned by the z_score function
 
+addcase = [462, 78]
 print("Z-scores of population 1", get_z_score_of_data(population1))
 print("Z-scores of population 2", get_z_score_of_data(population2))
 print("Z-scores of population 3", get_z_score_of_data(population3))
+print("Z-scores of additional 2cases", get_z_score_of_data(addcase))
